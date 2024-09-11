6a84fdb33a54177ba414969562f337f2044d23f9
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-09-10 21:37:57 -0300

    Added content to CustomHeader, styling to it and image to compose header

--- src/CustomHeader/CustomHeader.jsx
+++ src/CustomHeader/CustomHeader.jsx
@@ -3,7 +3,40 @@ import './CustomHeader.css';
 
 const CustomHeader = () => {
   return (
-    <div className="CustomHeader">Essa será a div que irá conter o cabeçalho da aplicação</div>
+    <div className="CustomHeader">
+      <ul className='HorizontalMenuItems'>
+        <li>
+          Blog
+        </li>
+        <li>
+          Sobre
+        </li>
+        <li>
+          Descrição
+        </li>
+      </ul>
+      <div className='Container'>
+        <div className='PhotoArea'>
+          <img
+            className='AvatarImage'
+            src={window.location.origin + '/Dickson.jpg'}
+            width={134.1}
+            height={168.5}
+          />
+        </div>
+        <div className='BioArea'>
+          <h1>Dickson Souza</h1>
+          <p className='Paragraph'>I am a metallurgical engineer! Coding and software development,
+            however, is on my life
+            for a long time!</p>
+          <p className='Paragraph'>
+            It all started as curiosity and it is gradually becoming something
+            more serious along the years, including a specialization course at PUC Minas on
+            Software Engineering
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
