282f5427ef05a98483f7dbe6fa4acbfddd3eff14
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-09-11 10:33:37 -0300

    Added content and styling of content with some links to my LinkedIn posts

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -5,28 +5,73 @@ const Content = () => {
   return (
     <div className={styles.Container}>
       <h1>Latest blog posts</h1>
-      <h2>5 dicas para sua carreira profissional</h2>
-      <p>Confira algumas dicas que podem ajudar a
-        alavancar sua carreira como
-        desenvolvedor front-end
+      <h2>
+        <a
+          href="https://www.linkedin.com/pulse/01-curating-information-promote-learning-dickson-alves-de-souza"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Curating information and promote learning
+        </a>
+      </h2>
+      <p>
+        <b>Information overload</b>, i.e., the widespread notion
+        that we can&apos;t grasp everything in the information environment and <i>FOMO</i>
+        are closely interconnected. Curating information can help.
+        <a
+          href="https://www.linkedin.com/pulse/01-curating-information-promote-learning-dickson-alves-de-souza"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <br />Read more...
+        </a>
       </p>
 
-      <h2>5 dicas para sua carreira profissional</h2>
-      <p>Confira algumas dicas que podem ajudar a
-        alavancar sua carreira como
-        desenvolvedor front-end
+      <h2>
+        <a
+          href="https://www.linkedin.com/pulse/02-slags-dickson-alves-de-souza"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Role of slags in metallurgy
+        </a>
+      </h2>
+      <p>
+        Slags, while a chemical by-product in metallurgy,
+        play important roles in metal refining.
+        Understanding them is essential for producing
+        several metals and alloys, like steels,
+        copper and even silicon used to make chips. <br />
+        <a
+          href="https://www.linkedin.com/pulse/02-slags-dickson-alves-de-souza"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more...
+        </a>
       </p>
 
-      <h2>5 dicas para sua carreira profissional</h2>
-      <p>Confira algumas dicas que podem ajudar a
-        alavancar sua carreira como
-        desenvolvedor front-end
-      </p>
-
-      <h2>5 dicas para sua carreira profissional</h2>
-      <p>Confira algumas dicas que podem ajudar a
-        alavancar sua carreira como
-        desenvolvedor front-end
+      <h2>
+        <a
+          href="https://www.linkedin.com/posts/disouzam_eaf-steelmaking-example-charging-optimization-activity-7224843565531619328-aWZA/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Using mathematical optimization in steelmaking
+        </a>
+      </h2>
+      <p>
+        This post shows a teaching example of
+        how to use OR-Tools to optimize
+        a meltshop operation in steelmaking.
+        <br />
+        <a
+          href="https://www.linkedin.com/posts/disouzam_eaf-steelmaking-example-charging-optimization-activity-7224843565531619328-aWZA/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more...
+        </a>
       </p>
 
     </div>
